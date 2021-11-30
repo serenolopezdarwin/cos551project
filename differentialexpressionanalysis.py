c9db51079208d817ba4a764096657411502838dc
serenolopezdarwin <sereno@princeton.edu>
2021-11-30 11:56:52 -0500

    Changed expression percentage by cell types to dictionary objects with original gene ids as keys.

--- differentialexpressionanalysis.py
+++ differentialexpressionanalysis.py
@@ -20,11 +20,10 @@ CELL_TYPES = ["Connective tissue progenitors", "Chondrocytes and osteoblasts", "
               "Cardiac muscle lineage", "Megakaryocytes", "Melanocytes", "Lens", "Neutrophils"]
 
 
-def build_gene_bidict(filt_mat: list) -> Bidict:
-    """Given a filtered matrix, builds a bidirectional dictionary linking the 2000 gene ids remaining after filtration
+def build_gene_bidict(gene_ids: list) -> Bidict:
+    """Given a list of gene ids, builds a bidirectional dictionary linking the 2000 gene ids remaining after filtration
     to the indices 0:1999."""
     gene_key = Bidict({})
-    gene_ids = sorted(list(set(filt_mat[0])))
     for idx, gene_id in enumerate(gene_ids):
         gene_key[gene_id] = idx
     return gene_key
@@ -40,7 +39,7 @@ def swap_gene_indexing(gene_ids: list, key: Bidict, to="standard") -> list:
     else:
         raise KeyError("Incorrect 'to' argument passed to gene id swapping function.")
     new_ids = []
-    for gene_id in enumerate(gene_ids):
+    for gene_id in gene_ids:
         new_id = applied_key[gene_id]
         if to == "standard":
             new_ids.append(new_id)
@@ -50,15 +49,17 @@ def swap_gene_indexing(gene_ids: list, key: Bidict, to="standard") -> list:
     return new_ids
 
 
-def count_gene_percentages(filt_mat: list, cell_data_dict: dict, gene_key: Bidict) -> dict:
+def count_gene_percentages(filt_mat: list, cell_data_dict: dict, gene_ids: list) -> dict:
     """Counts the percentage of cells each gene is expressed in, per-cluster. Returns a dict of each cell type (in
     order of our CELL_TYPES) and their corresponding expression percentage for each gene."""
     start = perf_counter()
     expression_counts = {}
     for cell_type in CELL_TYPES:
         # This will store the expressed and unexpressed counts for each of our genes. First entry is total count,
-        # second is expressed count.
-        expression_counts[cell_type] = [(0, 0)] * 2000
+        # second is expressed count. Needs to be a dict to be unique in memory and a list to be mutable.
+        expression_counts[cell_type] = {}
+        for gene_id in gene_ids:
+            expression_counts[cell_type][gene_id] = [0, 0]
     filt_mat_chunk_cell_path = "intermediates/filt_mat_chunk_cell.pkl"
     if os.path.exists(filt_mat_chunk_cell_path):
         with open(filt_mat_chunk_cell_path, 'rb') as filt_mat_by_cells_in:
@@ -77,18 +78,19 @@ def count_gene_percentages(filt_mat: list, cell_data_dict: dict, gene_key: Bidic
             continue
         # Builds a list of the genes expressed in this cell and translates their indexes to our 1-1999 standard.
         expressed_genes = []
-        # TODO: What exactly is this data structure? Unpacking gives a tuple as gene_id somehow.
         for gene_id, _ in cell_sparse_mat:
             expressed_genes.append(gene_id)
-        expressed_gene_idxs = swap_gene_indexing(expressed_genes, gene_key)
-        for gene_idx in range(0, 2000):
-            expression_counts[cell_type][gene_idx][0] += 1
-            if gene_idx in expressed_gene_idxs:
-                expression_counts[cell_type][gene_idx][1] += 1
+        # expressed_gene_idxs = swap_gene_indexing(expressed_genes, gene_key)
+        for gene_id in gene_ids:
+            expression_counts[cell_type][gene_id][0] += 1
+            if gene_id in expressed_genes:
+                expression_counts[cell_type][gene_id][1] += 1
     expression_percentages = {}
     for cell_type, expression_data in expression_counts.items():
-        expression_percentage = [exp / total for total, exp in expression_data]
-        expression_percentages[cell_type] = expression_percentage
+        expression_percentages[cell_type] = {}
+        for gene_id, gene_exp in expression_data.items():
+            expression_percentage = gene_exp[1] / gene_exp[0]
+            expression_percentages[cell_type][gene_id] = expression_percentage
     log(f"Expression percentages calculated in {str(perf_counter() - start)}")
     return expression_percentages
 
@@ -112,8 +114,8 @@ def main() -> None:
         with open(expression_percentage_path, 'rb') as exp_perc_in:
             expression_percentages = pkl.load(exp_perc_in)
     else:
-        gene_key = build_gene_bidict(filt_mat)
-        expression_percentages = count_gene_percentages(filt_mat, cell_data_dict, gene_key)
+        gene_ids = list(set(filt_mat[0]))
+        expression_percentages = count_gene_percentages(filt_mat, cell_data_dict, gene_ids)
         with open(expression_percentage_path, 'rb') as exp_perc_out:
             pkl.dump(expression_percentages, exp_perc_out)
 
